ca9edcc1aef2d791b231e062038f24032091931b
udit710 <s3933905@student.rmit.edu.au>
2024-12-19 12:51:28 +1100

    Added model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,8 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 # Define paths to save imported data
 @dataclass # Use dataclass only where class has variables defined in it, else use __init__
@@ -56,5 +58,8 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
     
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_path=train_data, test_path=test_data)
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_path=train_data, test_path=test_data)
+    
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr=train_arr, test_arr=test_arr))
     
